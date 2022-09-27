81d20cf7457595f7299d7410ed55107dd1412649
buentellopc <buentellopc@gmail.com>
2022-09-27 16:37:55 -0500

    Retrieving data from stackoverflow API

--- src/containers/Feed/Feed.tsx
+++ src/containers/Feed/Feed.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import styled from "styled-components";
 
 const FeedWrapper = styled.div`
@@ -17,7 +17,42 @@ const ROOT_API = "https://api.stackexchange.com/2.2/";
 interface FeedProps {}
 
 const Feed = () => {
-  return <FeedWrapper>Feed Component</FeedWrapper>;
+  const [stackData, setStackData] = useState<{ items: any[] }>(
+    {} as { items: any[] }
+  );
+  const [loading, SetLoading] = useState(true);
+  // const [error, SetError] = useState(null)
+
+  useEffect(() => {
+    const data = fetch(
+      `${ROOT_API}questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow`
+    );
+
+    async function startFetching() {
+      const data = await fetch(
+        `${ROOT_API}questions?order=desc&sort=activity&tagged=reactjs&site=stackoverflow`
+      );
+
+      console.log("here is the data from stack overflow", data);
+
+      const dataJson = await data.json();
+
+      console.log(dataJson);
+
+      setStackData((prevStackData) => dataJson);
+      SetLoading(false);
+    }
+
+    startFetching();
+  }, []);
+
+  return (
+    <FeedWrapper>
+      Questions from Stack Overflow will go here
+      {!loading &&
+        stackData.items.map((item, index) => <p key={index}>{item.title}</p>)}
+    </FeedWrapper>
+  );
 };
 
 export default Feed;
